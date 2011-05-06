7136d13ebb28a20d023e43c277d82bcf9e9ef34c
bolduc.sim <bolduc.sim@ce87ebec-3dc8-365c-5703-a7c7c116a326>
2011-05-06 00:33:52 +0000

    tri, création de l'arbre

--- Log320-Lab1/src/ca/etsmtl/huffman/HuffmanDecompress.java
+++ Log320-Lab1/src/ca/etsmtl/huffman/HuffmanDecompress.java
@@ -2,12 +2,25 @@ package ca.etsmtl.huffman;
 
 public class HuffmanDecompress {
 
+	private static final String TXT_EXTENSION = "txt";
+	private static final String HUF_EXTENSION = "huf";
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (args.length > 0) {
+			String fileName = args[0];
+			if (fileName.endsWith(HUF_EXTENSION)) {
 
+				
+				
+			} else {
+				System.out.println("Can't compress file, extension is not " + HUF_EXTENSION);
+			}
+		} else {
+			System.out.println("Can't compress file, name is not specified");
+		}
 	}
 
 }
