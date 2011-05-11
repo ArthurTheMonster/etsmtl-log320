6e144ed1740d8d83fbd10951e47ab3c4c9ac5fe4
bolduc.sim <bolduc.sim@ce87ebec-3dc8-365c-5703-a7c7c116a326>
2011-05-11 21:57:16 +0000

    encodage

--- trunk/Log320-Lab1/src/ca/etsmtl/huffman/HuffmanNode.java
+++ trunk/Log320-Lab1/src/ca/etsmtl/huffman/HuffmanNode.java
@@ -2,23 +2,32 @@ package ca.etsmtl.huffman;
 
 public class HuffmanNode implements Comparable<HuffmanNode> {
 
-	public int value;
 	public int frequency;
+	public int value;
+	public HuffmanNode left;
+	public HuffmanNode right;
 	
 	public HuffmanNode() {
 		this(0, 0);
 	}
 	
 	public HuffmanNode(int value, int frequency) {
+		this(value, frequency, null, null);
+	}
+	
+	public HuffmanNode(int value, int frequency, HuffmanNode left, HuffmanNode right) {
 		this.value = value;
 		this.frequency = frequency;
+		this.left = left;
+		this.right = right;
 	}
 	
 	@Override
 	public int compareTo(HuffmanNode otherHuffmanNode) {
 		int result = frequency - otherHuffmanNode.frequency;
+		//FIXME: tester
 		if (result == 0) {
-			return 1;
+			return 0;
 		} else {
 			return result;
 		}
