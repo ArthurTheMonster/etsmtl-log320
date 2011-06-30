e9be316f9c9b93de75e092e300b5072118cdd1bc
Yianks <Yianks@ce87ebec-3dc8-365c-5703-a7c7c116a326>
2011-06-30 04:59:26 +0000

    We suggest some move for the next round. Improve performance. Now we can afford 6 level deep in the tree in 5 seconds (woot!). Teacher is no challenge anymore, even at 10sec he get destroyed. Still many optimization on the implemention should be done. Also he suggestion are some time really stupid, probably a small bug there.

--- Log320-Lab3/src/ca/etsmtl/breaktrough/NinjaClient.java
+++ Log320-Lab3/src/ca/etsmtl/breaktrough/NinjaClient.java
@@ -5,6 +5,7 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NinjaClient {
@@ -34,9 +35,15 @@ public class NinjaClient {
 	
 	private Player maxPlayer = Player.BLACK;
 	private Player minPlayer = Player.WHITE;
-
 	
 	private Move myBestMove;
+	
+	private List<Move> listChoosedMove = new ArrayList<Move>();
+	private List<Move> listTempChoosedMove = new ArrayList<Move>();
+	
+	private long timeGameStarted = 0;
+	
+	private int maxPointForThisRound = Integer.MIN_VALUE;
 	
 	public NinjaClient() {
 		initTables();
@@ -61,6 +68,7 @@ public class NinjaClient {
 	}
 	
 	private void playBreaktrough() {
+		timeGameStarted = System.currentTimeMillis();
 		while (!gameCompleted) {
 			readOpponentMove();
 			buildDecisionTree();
@@ -92,78 +100,149 @@ public class NinjaClient {
 		}
 	}
 	
-	private int NinjaMax(GameTable table, int howManyMoveLeft, int alpha, int beta) {
+	private int NinjaMax(GameTable table, int howManyMovesLeft, int alpha, int beta) {
 		Move tempBestMove = null;
-
-		if (howManyMoveLeft == 0) {
+		
+		if (isMyLastMove(howManyMovesLeft)) {
 			return evalTable(table, maxPlayer);
 		}
-
-		howManyMoveLeft--;
 		
-		List<Move> listMove = GameTable.getAllMove(table, maxPlayer);
+		List<Move> listMove = new ArrayList<Move>();
+		List<Move> listGoodMoves = new ArrayList<Move>();
+		
+		// Last time we suggested some move for us for this round
+		if (isMyFirstMove(howManyMovesLeft)) {
+			// If the move is still valid
+			for (Move move : listChoosedMove) {
+				if (GameTable.isValidMove(gameTable, move)) {
+					// Play this move first
+					listMove.add(move);
+				}
+			}
+			listChoosedMove.clear();
+		}
+		
+		listMove.addAll(GameTable.getAllMove(table, maxPlayer));
 		
 		int currentAlpha = Integer.MIN_VALUE;
 		for (Move move : listMove) {
 			GameTable newGameTable = new GameTable(table);
+			
 			GameTable.move(newGameTable, move);
-			int score = NinjaMin(newGameTable, howManyMoveLeft,Math.max(alpha,currentAlpha),beta);
+			int score = NinjaMin(newGameTable, howManyMovesLeft-1,Math.max(alpha,currentAlpha),beta);
 
+			if (isMyFirstMove(howManyMovesLeft)) {
+				System.out.println("New score: " + score);
+			}
+			
 			/*if (table.getBlackPawnCount() > newGameTable.getBlackPawnCount()) {
 				score += 500;
 			}*/
 			
-			if (howManyMoveLeft == deepnessTree-1) {
-				System.out.println("NEW MOVE: " + score);
-			}
-			
-			
+			// This is the best score so far						
 			if (score > currentAlpha) {
 				currentAlpha = score;
 				tempBestMove = move;
-
-				if (currentAlpha > beta) {
-					myBestMove = tempBestMove;
+				
+				// In fact, I can play so good that I don't expect the opponent to 
+				// be enough stupid do the last move, let just leave.
+				if (currentAlpha >= beta) {					
 					return currentAlpha;
+				}
+								
+				// Let remember that move for the next round
+				if (isMySecondMove(howManyMovesLeft)) {
+					listGoodMoves.add(move);
+				}
+				
+				// This is the score to be beaten in my second move to be saved.
+				if (isMyFirstMove(howManyMovesLeft)) {
+					maxPointForThisRound = score;
 				}
 			}
 		}
 		
-		if (howManyMoveLeft == deepnessTree-1) {
-			System.out.println("CHOOSED MOVE:" + currentAlpha);
+		// I can expect him to play his last move and this result his good. Let save it for next round
+		if (isMySecondMove(howManyMovesLeft) && currentAlpha > maxPointForThisRound && // Je veux etre retenu par MaxNinja
+				currentAlpha < beta) { // Je veux être retenue par MinNinja
+			listTempChoosedMove = listGoodMoves;
 		}
-		myBestMove = tempBestMove;
+		
+		// Let save the move we should play
+		if (isMyFirstMove(howManyMovesLeft)) {
+			myBestMove = tempBestMove;
+			System.out.println("CHOOSED MOVE:" + currentAlpha);
+			maxPointForThisRound = Integer.MIN_VALUE;
+			for (Move move : listChoosedMove) {
+				System.out.println("I suggest this move: " + move.toString());
+			}
+		}
+		
 		return currentAlpha;
 	}
 	
-	private int NinjaMin(GameTable table, int howManyMoveLeft, int alpha, int beta) {	
-		if (howManyMoveLeft == 0) {
+	private int NinjaMin(GameTable table, int howManyMovesLeft, int alpha, int beta) {	
+		if (isMyLastMove(howManyMovesLeft)) {
 			return evalTable(table, maxPlayer);
 		}
-
-		howManyMoveLeft--;
-		
+			
 		List<Move> listMove = GameTable.getAllMove(table, minPlayer);
 		
 		int currentBeta = Integer.MAX_VALUE;
 		for (Move move : listMove) {
 			GameTable newGameTable = new GameTable(table);
+	
 			GameTable.move(newGameTable, move);
-			int score = NinjaMax(newGameTable, howManyMoveLeft,alpha,Math.min(beta,currentBeta));
+			int score = NinjaMax(newGameTable, howManyMovesLeft-1,alpha,Math.min(beta,currentBeta));
 			
 			/*if (table.getWhitePawnCount() > newGameTable.getWhitePawnCount()) {
 				score -= 500;
 			}*/
 			
+			// This is the best score so far
 			if (score < currentBeta) {
 				currentBeta = score;
 				
-				if (currentBeta < alpha) {
+				// In fact, he can play so good that I will never do that
+				// last move, let just leave.
+				if (currentBeta <= alpha) {
 					return currentBeta;
 				}
 			}
 		}
+		
+		// This move is the best so far
+		if (isOppFirstMove(howManyMovesLeft) && currentBeta > maxPointForThisRound) {
+			listChoosedMove = listTempChoosedMove;
+		}
 		return currentBeta;
+	}
+
+	// TODO: To be removed
+	// We should never get stuck in this function.
+	private void isValid(GameTable newGameTable, Move move) {
+		if (!GameTable.isValidMove(newGameTable, move)) {
+			while (true) {
+				System.out.println("INVALID MOVE");
+				GameTable.printGameTable(newGameTable);
+				System.out.println(move.toString());
+			}
+		}
+	}
+	
+	private boolean isMyFirstMove(int howManyMovesLeft) {
+		return howManyMovesLeft == deepnessTree;
+	}
+
+	private boolean isMySecondMove(int howManyMovesLeft) {
+		return howManyMovesLeft == deepnessTree-2;
+	}
+	private boolean isOppFirstMove(int howManyMovesLeft) {
+		return howManyMovesLeft == deepnessTree-1;
+	}
+	
+	private boolean isMyLastMove(int howManyMovesLeft) {
+		return howManyMovesLeft == 0;
 	}
 	
 	private int evalTable(GameTable table, Player player) {
@@ -189,7 +268,7 @@ public class NinjaClient {
 		}
 		System.out.println("Oh. Btw. Our awsome transposition table has now: " + GameTable.transposition.size() + " entries.");
 		nbMovePlayed++;
-		System.out.println(System.currentTimeMillis() - time);
+		System.out.println("Last move time : " + ((int)System.currentTimeMillis() - (int)time) + " Game time elapsed: " + ((int)System.currentTimeMillis() - (int)timeGameStarted));
 		try {
 			GameTable.move(gameTable, myBestMove);
 			output.write(myBestMove.toString().getBytes(), 0, 7);
