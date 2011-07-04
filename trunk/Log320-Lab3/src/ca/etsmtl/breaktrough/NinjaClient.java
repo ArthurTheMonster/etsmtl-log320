35dd92a60933b03f5ddc9608be102bf413f72907
Yianks <Yianks@ce87ebec-3dc8-365c-5703-a7c7c116a326>
2011-07-04 04:53:42 +0000

    The brain is now working

--- trunk/Log320-Lab3/src/ca/etsmtl/breaktrough/NinjaClient.java
+++ trunk/Log320-Lab3/src/ca/etsmtl/breaktrough/NinjaClient.java
@@ -5,9 +5,6 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.List;
 
 public class NinjaClient {
 
@@ -23,6 +20,8 @@ public class NinjaClient {
 	
 	private String[] firstMoves = { "E2 - E3", "E3 - E4" };
 	
+	private long TIME_TO_THINK = 5750;
+	
 	private int nbMovePlayed = 0;
 	
 	private byte[] opponentMoveBuffer = new byte[16];
@@ -32,30 +31,18 @@ public class NinjaClient {
 	
 	private GameTable gameTable = new GameTable();
 	
-	private int deepnessTree = 7;
+	private int defaultDeepnessTree = 6;
 	
 	private Player maxPlayer = Player.BLACK;
 	private Player minPlayer = Player.WHITE;
 	
-	private Move myBestMove;
-	
 	private long timeGameStarted = 0;
 	
-	private Hashtable<Long, Move> whatIShouldPlay = new Hashtable<Long, Move>();
-	private Hashtable<Long, Integer> whatIShouldPlayPoint = new Hashtable<Long, Integer>();
-
-	private Move choosedMove;
-	private Move tempChoosedMove;
-	private int tempChoosedMovePoint;
+	TheBrain brain = new TheBrain(maxPlayer, minPlayer);
 	
 	public NinjaClient() {
-		initTables();
 		initCommunication();
 		playBreaktrough();
-	}
-	
-	private void initTables() {
-		//TODO: load tables
 	}
 	
 	private void initCommunication() {
@@ -74,6 +61,7 @@ public class NinjaClient {
 		timeGameStarted = System.currentTimeMillis();
 		while (!gameCompleted) {
 			readOpponentMove();
+			brain.stop();
 			buildDecisionTree();
 			sendMove();
 		}
@@ -90,7 +78,7 @@ public class NinjaClient {
 					minPlayer = Player.BLACK;
 				} else {
 					lastOpponentMove = parseMove(new String(opponentMoveBuffer), minPlayer);
-					GameTable.move(gameTable, lastOpponentMove);
+					gameTable.move(lastOpponentMove);
 				}
 
 			} else if (cmd == '2') {
@@ -103,153 +91,7 @@ public class NinjaClient {
 		}
 	}
 	
-	private int NinjaMax(GameTable table, int howManyMovesLeft, int alpha, int beta) {
-		Move tempBestMove = null;
 		
-		if (isMyLastMove(howManyMovesLeft)) {
-			return evalTable(table, maxPlayer);
-		}
-		
-		List<Move> listMove = new ArrayList<Move>();
-
-		// Try the move we suggested for this attack
-		if (isMyFirstMove(howManyMovesLeft)) {
-			// We are ready man!
-			if (whatIShouldPlay.containsKey(table.getTable())) {
-				Move move = whatIShouldPlay.get(table.getTable());
-				if (table.isValidMove(move)) {
-					listMove.add(move);	
-					System.out.println("I have been suggest to do this move: " + move.toString());
-				}
-			}
-		}
-		
-		listMove.addAll(GameTable.getAllMove(table, maxPlayer));
-		
-		int currentAlpha = Integer.MIN_VALUE;
-		for (Move move : listMove) {
-			GameTable newGameTable = new GameTable(table);
-			
-			GameTable.move(newGameTable, move);
-			int score = NinjaMin(newGameTable, howManyMovesLeft-1,Math.max(alpha,currentAlpha),beta);
-
-			if (isMyFirstMove(howManyMovesLeft)) {
-				System.out.println("New score: " + score);
-			}
-			
-			/*if (table.getBlackPawnCount() > newGameTable.getBlackPawnCount()) {
-				score += 500;
-			}*/
-			
-			// This is the best score so far						
-			if (score > currentAlpha) {
-				currentAlpha = score;
-				tempBestMove = move;
-				
-				// Let remember that move for the next round
-				if (isMySecondMove(howManyMovesLeft)) {
-					tempChoosedMove = new Move(tempBestMove);
-					tempChoosedMovePoint = currentAlpha;
-				}
-				
-				// In fact, I can play so good that I don't expect the opponent to 
-				// be enough stupid do the last move, let just leave.
-				if (currentAlpha >= beta) {					
-					return currentAlpha;
-				}
-			}
-		}
-		
-		// Let save the move we should play
-		if (isMyFirstMove(howManyMovesLeft)) {
-			myBestMove = tempBestMove;
-			System.out.println("CHOOSED MOVE: " + myBestMove.toString() + " - Points: " + currentAlpha);
-			// Move to suggest
-		}
-		
-		return currentAlpha;
-	}
-	
-	private int NinjaMin(GameTable table, int howManyMovesLeft, int alpha, int beta) {	
-		if (isMyLastMove(howManyMovesLeft)) {
-			return evalTable(table, maxPlayer);
-		}
-			
-		List<Move> listMove = GameTable.getAllMove(table, minPlayer);
-		
-		int currentBeta = Integer.MAX_VALUE;
-		for (Move move : listMove) {
-			GameTable newGameTable = new GameTable(table);
-	
-			GameTable.move(newGameTable, move);
-			int score = NinjaMax(newGameTable, howManyMovesLeft-1,alpha,Math.min(beta,currentBeta));
-			
-			/*if (table.getWhitePawnCount() > newGameTable.getWhitePawnCount()) {
-				score -= 500;
-			}*/
-			
-			if (isOppFirstMove(howManyMovesLeft)){
-				long ltable = newGameTable.getTable();
-				if (whatIShouldPlay.containsKey(ltable)) {
-					if (tempChoosedMovePoint > whatIShouldPlayPoint.get(ltable)) {
-						whatIShouldPlay.remove(ltable);
-						whatIShouldPlay.put(ltable, tempChoosedMove);
-						
-						whatIShouldPlayPoint.remove(ltable);
-						whatIShouldPlayPoint.put(ltable, tempChoosedMovePoint);
-					}
-				} else {
-					whatIShouldPlay.put(ltable, tempChoosedMove);					
-					whatIShouldPlayPoint.put(ltable, tempChoosedMovePoint);			
-				}
-			}
-			
-			// This is the best score so far
-			if (score < currentBeta) {
-				currentBeta = score;
-				
-				// In fact, he can play so good that I will never do that
-				// last move, let just leave.
-				if (currentBeta <= alpha) {
-					return currentBeta;
-				}
-			}
-		}
-		
-		return currentBeta;
-	}
-
-	// TODO: To be removed
-	// We should never get stuck in this function.
-	private void isValid(GameTable newGameTable, Move move) {
-		if (!newGameTable.isValidMove(move)) {
-			while (true) {
-				System.out.println("INVALID MOVE");
-				GameTable.printGameTable(newGameTable);
-				System.out.println(move.toString());
-			}
-		}
-	}
-	
-	private boolean isMyFirstMove(int howManyMovesLeft) {
-		return howManyMovesLeft == deepnessTree;
-	}
-
-	private boolean isMySecondMove(int howManyMovesLeft) {
-		return howManyMovesLeft == deepnessTree-2;
-	}
-	private boolean isOppFirstMove(int howManyMovesLeft) {
-		return howManyMovesLeft == deepnessTree-1;
-	}
-	
-	private boolean isMyLastMove(int howManyMovesLeft) {
-		return howManyMovesLeft == 0;
-	}
-	
-	private int evalTable(GameTable table, Player player) {
-		return table.getTableScore(player);
-	}
-	
 	private Move parseMove(String opponentMove, Player player) {
 		String[] move = opponentMove.trim().split(" - ");
 		return new Move(move[0], move[1], player);
@@ -259,17 +101,36 @@ public class NinjaClient {
 	}
 	
 	private void sendMove() {
-		long time = System.currentTimeMillis();
-		if (nbMovePlayed < firstMoves.length) {
-			myBestMove = parseMove(firstMoves[nbMovePlayed], maxPlayer);
-		} else {
-			NinjaMax(gameTable, deepnessTree,Integer.MIN_VALUE,Integer.MAX_VALUE);
+		brain = new TheBrain(maxPlayer, minPlayer);
+			
+		long timeStartThinking = System.currentTimeMillis();			
+		
+		brain.prepareTheBrain(gameTable, defaultDeepnessTree);
+		brain.start();
+		
+		while (true) {
+			if (System.currentTimeMillis() - timeStartThinking > TIME_TO_THINK) {
+				break;
+			}	
 		}
-		System.out.println("Oh. Btw. Our awsome transposition table has now: " + GameTable.transposition.size() + " entries.");
-		nbMovePlayed++;
-		System.out.println("Last move time : " + ((int)System.currentTimeMillis() - (int)time) + " Game time elapsed: " + ((int)System.currentTimeMillis() - (int)timeGameStarted));
+
+		Move myBestMove = brain.getBestMove();
+		if (myBestMove == null) { myBestMove = brain.mySuggestedMove(); }
+		
+		int deepnessThinking = brain.howSmartAmI();
+		
+		if (nbMovePlayed < firstMoves.length) {
+			System.out.println("Oh we had a preset for this move, we use it.");
+			myBestMove = parseMove(firstMoves[nbMovePlayed], maxPlayer);
+			deepnessThinking = 0;
+		} else {
+			
+		}
+		System.out.println("CHOOSED MOVE: " + myBestMove.toString() + " - Deepness: " + deepnessThinking);
+		System.out.println("Last move time : " + ((int)System.currentTimeMillis() - (int)timeStartThinking) + " Game time elapsed: " + ((int)System.currentTimeMillis() - (int)timeGameStarted)+ "\n");
 		try {
-			GameTable.move(gameTable, myBestMove);
+			gameTable.move(myBestMove);
+			nbMovePlayed++;
 			output.write(myBestMove.toString().getBytes(), 0, 7);
 			output.flush();
 		} catch (IOException e) {
